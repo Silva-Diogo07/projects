8bbe675d3989e40479e74910b83a83cad27dff49
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2025-12-14 02:23:18 +0000

    Add final.c

--- C/Makefile/final.c
+++ C/Makefile/final.c
@@ -4,27 +4,71 @@
 #include <stdlib.h>
 #include <time.h>
 
-int max(int max)
+
+void numero_secreto(int *secret)
 {
+    int max;
+
     printf("Qual será o número máximo? ");
     scanf("%d" , &max);
-}
 
-int numero_secreto(int secret , int max)
-{
-    // Muda a seed para o número inicial não ser sempre o mesmo
+    // Muda a seed para o números não se repetirem ao iniciar o programa não ser sempre o mesmo
     srand(time(NULL));
     // Picks a randomized number
-    secret = rand() % (max + 1);
-    scanf("%d" , &secret);
+    *secret = rand() % (max + 1);
+}
+
+void pedir_tentativas(int *tentativas)
+{
+    printf("Quantas tentativas? ");
+    scanf("%d" , tentativas);
+}
+
+void pedir_palpite(int *palpite)
+{
+    printf("Qual é o teu palpite? ");
+    scanf("%d" , palpite);
+}
+
+void verificar_palpite(int *palpite , int *secret , int *tentativas)
+{
+    if (*palpite > *secret)
+    {
+        printf("Mais Baixo\n");
+        *tentativas --;
+        printf("Tens %d restantes.\n" , *tentativas);
+    }
+    else if (*palpite < *secret)
+    {
+        printf("Mais Alto\n");
+        *tentativas --;
+        printf("Tens %d tentativas restantes.\n" , *tentativas);
+    }
+    else
+    {
+        printf("Acertaste\n");
+    }
 }
 
 int main(void)
 {
-    printf("Debug | O número máximo é : %d" , max);
+    int tentativas;
+    int palpite;
+    int secret;
+    numero_secreto(&secret);
+    pedir_tentativas(&tentativas);
+
+    while (tentativas > 0)
+    {
+        pedir_palpite(&palpite);
+        verificar_palpite(&palpite , &secret , &tentativas);
+    }
+    printf("\n");
+    printf("Debug\n");
+    printf("Número Secreto : %d" , secret);
 }
 
 
-// scanf("formato" , &variavel)
-// exemplo :
-// scanf("%d" , &tries)
+// TODO : Fix tries output bug 
+// TODO : playAgain
+
