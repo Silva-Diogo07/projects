e1ab97dc81574c6206a36729cdda1488f88ca899
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2025-12-26 23:09:25 +0000

    Study

--- C/Makefile/phonebook.c
+++ C/Makefile/phonebook.c
@@ -1,9 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    char *name;
+    char *number;
+} person;
+
+
 int main(void)
 {
-    char *names[] = {"Tone", "Michael", "Biclas"};
-    char *numbers[] = {"912-122-130", "301-111-321", "912-122-130"};
+    person people[3];
     
+    people[0].name = "Michael";
+    people[0].number = "+351 910 329 102";
+
+    people[1].name = "Tone";
+    people[1].number = "+351 961 491 103";
+
+    people[2].name = "Ana";
+    people[2].number = "+351 910 329 102";
+
+    char name[100]; 
+
+    printf("Name: ");
+    scanf("%99s", name);
+
+    for (int i = 0; i < 3; i++)
+    {   
+        if (strcmp(people[i].name, name) == 0)
+        {
+            printf("Found.\nNumber : %s\n", people[i].number);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
