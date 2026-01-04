2054606733963f7cd7ce91ffe3492013cb506883
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-04 17:40:16 +0000

    Add notes

---
+++ Notes/f-variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float avaliação = 4.5;
+    float preço = 14.49;
+    float temperatura = 12.6;
+
+    printf("A avaliação foi %f\n", avaliação);
+    printf("O preço é £%f\n", preço);
+    printf("Lá fora estão %fºC\n", temperatura);
+
+    return 0;
+}
